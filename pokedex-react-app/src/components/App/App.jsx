4df39c320aa8f98384a3441ed5da934b53dc2734
Andrea Massa <andreamassa1999@gmail.com>
2023-09-23 12:08:24 -0400

    Added Boostrap, Cleared file structure

--- pokedex-react-app/src/components/App/App.jsx
+++ pokedex-react-app/src/components/App/App.jsx
@@ -1,10 +1,12 @@
+import PokedexSearch from '../PokedexSearch/PokedexSearch'
 import './App.css'
+
 
 function App() {  
 
   return (
     <div className='App'>
-
+      <PokedexSearch/>
     </div>
   )
 }
---
+++ pokedex-react-app/src/components/PokedexSearch/PokedexSearch.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import './PokedexSearch.css'
+
+function PokedexSearch(){
+    return(
+        <div className="pokedex-search">
+            <h1>Pokedex</h1>
+        </div>
+    )
+}
+
+export default PokedexSearch
