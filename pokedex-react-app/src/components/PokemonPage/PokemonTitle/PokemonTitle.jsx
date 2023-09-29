9859b5070e3d200452313f785557113449b11285
Andrea Massa <andreamassa1999@gmail.com>
2023-09-29 14:00:36 -0400

    Minor CSS Changes pt 2

--- pokedex-react-app/src/components/PokemonPage/PokemonTitle/PokemonTitle.jsx
+++ pokedex-react-app/src/components/PokemonPage/PokemonTitle/PokemonTitle.jsx
@@ -4,7 +4,9 @@ import "./PokemonTitle.css"
 function PokemonTitle(props){
     return(
         <h1 className="pokemon-title">
-            <span className="entry-number">{props.entryNumber}</span>
+            <span 
+                style={props.entryNumber < 100 ? {padding: '10px 20px'} : {padding: '15px'}}
+                className="entry-number">{props.entryNumber}</span>
             <span className="entry-name">{props.entryName}</span>
         </h1>
     )
