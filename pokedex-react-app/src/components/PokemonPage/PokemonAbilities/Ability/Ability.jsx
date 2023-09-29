ab031f7226566a8583441f19857024053d625e98
Andrea Massa <andreamassa1999@gmail.com>
2023-09-29 12:41:54 -0400

    Ability Component Done, added dependency (react-icon)

--- pokedex-react-app/src/components/PokemonPage/PokemonAbilities/Ability/Ability.jsx
+++ pokedex-react-app/src/components/PokemonPage/PokemonAbilities/Ability/Ability.jsx
@@ -1,10 +1,16 @@
 import "./Ability.css";
+import {BsFillEyeSlashFill, BsFillEyeFill} from 'react-icons/bs'
 
 function Ability(props){
     return(
         <li className="ability">
             <span className="isHidden">
-                
+                {props.isHidden 
+                ? 
+                    <BsFillEyeSlashFill/>
+                : 
+                    <BsFillEyeFill/>
+                }
             </span>
             <span className="abilityName">{props.name}{props.isHidden}</span>
         </li>
