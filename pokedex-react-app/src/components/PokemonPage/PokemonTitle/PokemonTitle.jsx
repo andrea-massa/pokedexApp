3f43335d4654542954900b3f50174abc7515a174
Andrea Massa <andreamassa1999@gmail.com>
2023-11-13 13:45:49 -0500

    Fixed Pokemon Number not being centered Bug

--- pokedex-react-app/src/components/PokemonPage/PokemonTitle/PokemonTitle.jsx
+++ pokedex-react-app/src/components/PokemonPage/PokemonTitle/PokemonTitle.jsx
@@ -10,7 +10,6 @@ function PokemonTitle(props){
     return(
         <h1 className="pokemon-title">
             <span 
-                style={props.entryNumber < 100 ? {padding: '10px 20px'} : {padding: '15px'}}
                 className="entry-number">{props.entryNumber}
             </span>
             <span className="entry-name">
