3f842e833ae2d6d1a9be51340271d89a4f0dcce9
Andrea Massa <andreamassa1999@gmail.com>
2023-11-13 19:18:55 -0500

    Search Box more Reactive

--- pokedex-react-app/src/components/PokedexSearch/SearchBar/SearchBar.jsx
+++ pokedex-react-app/src/components/PokedexSearch/SearchBar/SearchBar.jsx
@@ -1,43 +1,75 @@
-import { useState } from "react"
-import { IoSearchCircle, IoSearchOutline } from "react-icons/io5"
+import { useState, useEffect, useRef } from "react"
+import { IoDuplicateOutline, IoSearchCircle, IoSearchOutline } from "react-icons/io5"
 
 import "./SearchBar.css"
 
 
 
 function SearchBar(props){
-
+    
     // State
     const [isBarExpanded, setIsBarExpanded] = useState(false)    
     const [currentSearchBarValue, setCurrentSearchBarValue] = useState('')
+    
+
+    //Effect, whenever the user focuses outside the search box or scrolls, the search box shall collapse
+    useEffect(() => {             
+        const searchBarInput = document.getElementById('search-bar-input')    
+
+        function handleFocusOut(){            
+            setTimeout(() => {setIsBarExpanded(false)}, 100)
+        }
+
+        if(isBarExpanded && searchBarInput){
+            searchBarInput.focus()            
+            searchBarInput.addEventListener('focusout', handleFocusOut)
+            document.addEventListener('scroll', handleFocusOut)
+        }        
+        
+        return () =>{
+            if(searchBarInput){
+                searchBarInput.removeEventListener('focusout', handleFocusOut)
+                document.removeEventListener('scroll', handleFocusOut)
+            }            
+        }
+
+    }, [isBarExpanded])
 
 
+    function handleSearch(){
+        if(currentSearchBarValue === 'giratina'){
+            props.onSubmit('giratina-altered')
+        }
+        else{
+            props.onSubmit(currentSearchBarValue.toLowerCase())   
+        }                        
+        setCurrentSearchBarValue('')
+        setIsBarExpanded(false)
+    }
+
+    
     // JSX
     return(
-        <div className="container-fluid search-bar-container">
+        <div className="container-fluid search-bar-container" id="search-bar">
 
             {/* Search Bar Expanded State */}
             {isBarExpanded ?
-            <div className="col-12 col-lg-6 search-bar expanded">
+            <div 
+                className="col-12 col-lg-6 search-bar expanded"
+                >                
                 <input 
-                    class="form-control form-control-lg" 
+                    className="form-control form-control-lg" 
+                    id="search-bar-input"
                     type="text" 
                     placeholder="Pokemon Name or Number Ex: Ditto" 
                     aria-label=".form-control-lg example"
                     value={currentSearchBarValue}
-                    onChange={(e) => {setCurrentSearchBarValue(e.target.value)}}/> 
+                    onChange={(e) => {setCurrentSearchBarValue(e.target.value)}}
+                    onKeyDown={(e) => {if(e.key === 'Enter') handleSearch()}}
+                    /> 
                 <button 
                     className="btn btn-primary search-button"
-                    onClick={(e) => {
-                        if(currentSearchBarValue === 'giratina'){
-                            props.onSubmit('giratina-altered')
-                        }
-                        else{
-                            props.onSubmit(currentSearchBarValue.toLowerCase())   
-                        }                        
-                        setCurrentSearchBarValue('')
-                        setIsBarExpanded(false)
-                    }}>
+                    onClick={(e) => {handleSearch()}}>
                     <IoSearchOutline
                         className="search-icon"
                     />
