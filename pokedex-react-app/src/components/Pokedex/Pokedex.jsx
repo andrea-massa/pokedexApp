713a54dd9150431e84b7f66605d5ad20bcdddc46
Andrea Massa <andreamassa1999@gmail.com>
2023-11-16 12:34:06 -0500

    Added Router Template on main

---
+++ pokedex-react-app/src/components/Pokedex/Pokedex.jsx
@@ -0,0 +1,9 @@
+function Pokedex(){
+    return(
+        <div className="pokedex">
+            Hello
+        </div>
+    )
+}
+
+export default Pokedex
--- pokedex-react-app/src/main.jsx
+++ pokedex-react-app/src/main.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+
 import App from './components/App/App.jsx'
+import Pokedex from './components/Pokedex/Pokedex.jsx'
+import PokedexSearch from './components/PokedexSearch/PokedexSearch.jsx'
+
+
 import './index.css'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {
+        path: "/pokedex",
+        element: <Pokedex/>
+      },
+      {
+        path: "/pokemon/:pokemonId",
+        element: <PokedexSearch/>
+      }
+    ]        
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
