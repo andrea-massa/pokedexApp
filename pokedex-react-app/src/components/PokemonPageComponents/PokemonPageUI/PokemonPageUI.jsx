abda80c611b601ff95985473e8b545abe6b1efa3
Andrea Massa <andreamassa1999@gmail.com>
2024-01-10 12:02:20 -0500

    Bug fixed and UI layout improvde

--- pokedex-react-app/src/components/PokemonPageComponents/PokemonPageUI/PokemonPageUI.jsx
+++ pokedex-react-app/src/components/PokemonPageComponents/PokemonPageUI/PokemonPageUI.jsx
@@ -10,7 +10,7 @@ function PokemonPageUI({current}){
         <div className="container-fluid pokemon-page-ui">
             <div id='arrows-ui'>
                 {current > 1 && <Arrow current={current} type="prev"/>}                
-                {current < 1008 && <Arrow current={current} type="next"/>}                
+                {current < 1008 && <Arrow current={current}type="next"/>}                
             </div>
         </div>
     )
