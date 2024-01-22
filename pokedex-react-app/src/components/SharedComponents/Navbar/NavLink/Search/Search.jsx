e730d8bc3381af0dec842f5ade14f5f0c24befc0
Andrea Massa <andreamassa1999@gmail.com>
2024-01-22 18:17:02 -0500

    2. Search Bar Subcomponent with lifted state to parent

--- pokedex-react-app/src/components/SharedComponents/Navbar/NavLink/Search/Search.jsx
+++ pokedex-react-app/src/components/SharedComponents/Navbar/NavLink/Search/Search.jsx
@@ -1,15 +1,27 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+
+import SearchBar from "./SearchBar/SearchBar"
+
+import "./Search.css"
+
 
 
 export default function Search(){
     // Array containing all the Pokemon
-    let [allPokemon, setAllPokemon] = useState([])
+    let [allPokemon, setAllPokemon] = useState([]);
+    // Current Search Query State
+    let [searchQuery, setSearchQuery] = useState('');
 
     // Async function that calls the API 
     async function getAllPokemonData(){
         let response = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1018');
         let jsonData = await response.json()
         return jsonData
+    }
+
+    // Function that is passed as a prop to handle the search bar change
+    function handleSearchBarChange(value){
+        setSearchQuery(value);        
     }
 
     // Use Effect calls the PokeApi and gets all the Pokemon Data and 
@@ -29,7 +41,9 @@ export default function Search(){
 
     return(        
         <div className="search">            
-            
+            <SearchBar
+                onChange={handleSearchBarChange}/>
+            <p>{searchQuery}</p>
         </div>
     )
 }
