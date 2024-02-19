f0bbb1297c6c4ed45698660b0528c13b7f867f0e
Andrea Massa <andreamassa1999@gmail.com>
2024-02-19 17:23:20 -0500

    Added CSS layout colors and font for footer

--- pokedex-react-app/src/components/SharedComponents/Footer/Footer.jsx
+++ pokedex-react-app/src/components/SharedComponents/Footer/Footer.jsx
@@ -1,12 +1,14 @@
 import "./Footer.css"
 
+
+
 export default function Footer(){
     return (
-        <footer className="footer border text-center">
+        <footer className="footer text-center">
             <section className="footer-section container-md" id="author-section">
                 <h4>© 2024 Andrea Massa</h4>
                 <p>andreamassa1999@gmail.com</p>
-                <div className="container-sm border" id="links-container">
+                <div className="container-sm" id="links-container">
                     <a href="">Official Website</a>
                     <a href="">LinkedIn</a>
                     <a href="">GitHub</a>
