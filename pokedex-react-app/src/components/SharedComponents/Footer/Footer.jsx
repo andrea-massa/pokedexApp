275d547248a73351143bd057af619a61c77b7aa4
Andrea Massa <andreamassa1999@gmail.com>
2024-02-19 18:01:21 -0500

    Added images to working links

--- pokedex-react-app/src/components/SharedComponents/Footer/Footer.jsx
+++ pokedex-react-app/src/components/SharedComponents/Footer/Footer.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 import bitmoji from "../../../assets/bitmoji-smile.png";
+import linkedinLogo from "../../../assets/linkedin-logo.png";
+import githubLogo from "../../../assets/github.png";
+
 import "./Footer.css";
 
 
@@ -12,10 +15,17 @@ export default function Footer(){
                 <p>andreamassa1999@gmail.com</p>
                 <div className="container-sm" id="links-container">
                     <a className="footer-link" href="https://andreamassa.com/" target="blank">
-                        <img src={bitmoji} alt="official-website"/>
+                        <img className="img-fluid" src={bitmoji} alt="official-website"/>
+                        <p>Official Website</p>
                     </a>
-                    <a className="footer-link" href="https://www.linkedin.com/in/andrea-massa-48381115b/" children target="blank">LinkedIn</a>
-                    <a className="footer-link" href="https://github.com/andrea-massa" target="blank">GitHub</a>
+                    <a className="footer-link" href="https://www.linkedin.com/in/andrea-massa-48381115b/" children target="blank">
+                        <img className="img-fluid" src={linkedinLogo} alt="linkedin"/>
+                        <p>LinkedIn</p>
+                    </a>
+                    <a className="footer-link" href="https://github.com/andrea-massa" target="blank">
+                        <img className="img-fluid" src={githubLogo} alt="github"/>
+                        <p>Github</p>
+                    </a>
                 </div>
             </section>
             <section className="footer-section container-md" id="credits-section">
