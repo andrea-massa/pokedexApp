62a7e369fa03ce7a8d994cb0ea8c8ae59a2197ed
Andrea Massa <andreamassa1999@gmail.com>
2024-02-19 17:14:23 -0500

    Provided Footer HTML

--- pokedex-react-app/src/components/SharedComponents/Footer/Footer.jsx
+++ pokedex-react-app/src/components/SharedComponents/Footer/Footer.jsx
@@ -2,8 +2,20 @@ import "./Footer.css"
 
 export default function Footer(){
     return (
-        <footer className="footer border">
-            Footer
+        <footer className="footer border text-center">
+            <section className="footer-section container-md" id="author-section">
+                <h4>© 2024 Andrea Massa</h4>
+                <p>andreamassa1999@gmail.com</p>
+                <div className="container-sm border" id="links-container">
+                    <a href="">Official Website</a>
+                    <a href="">LinkedIn</a>
+                    <a href="">GitHub</a>
+                </div>
+            </section>
+            <section className="footer-section container-md" id="credits-section">
+                <h5>Attributions & Notices</h5>
+                <p>Pokemon Data provided by <a href="">PokeApi v2 by Nintendo</a></p>
+            </section>
         </footer>
     )
 }
